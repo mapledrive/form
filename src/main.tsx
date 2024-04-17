157e40e85516335a8147e4ce22f40d2ce1415cc7
mapledrive <sait.sharifullin@gmail.com>
2024-04-17 14:53:04 +0500

    structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const App: React.FC = () => {
+  return (
+    <div className='input-group'>
+      <label htmlFor='name'>Назначение</label>
+      <input type='text' id='name' name='name' />
+      <label htmlFor='length'>Длина</label>
+      <input type='text' id='length' name='length' />
+    </div>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
